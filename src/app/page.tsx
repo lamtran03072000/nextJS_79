17889639f820d8c09de6a0a1639314b430dd305c
Lâm Trần <118752371+lamtran03072000@users.noreply.github.com>
2025-03-27 19:17:21 +0700

    feat: add seo for page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,46 @@
 import Image from "next/image";
 
+export const metadata = {
+  title: "Shoes App - Lâm",
+  description:
+    "Explore our wide range of shoes with the best prices and quality.",
+  openGraph: {
+    title: "Shoes App - facebook - lâm trần",
+    description:
+      "Explore our wide range of shoes with the best prices and quality.",
+    url: "https://shoesshopbc70.vercel.app",
+    images: [
+      {
+        url: "https://apistore.cybersoft.edu.vn/images/van-old-school.png",
+        width: 800,
+        height: 600,
+        alt: "Shoes App",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Shoes App - Home",
+    description:
+      "Explore our wide range of shoes with the best prices and quality.",
+    images: ["https://apistore.cybersoft.edu.vn/images/van-old-school.png"],
+  },
+  jsonLd: {
+    "@context": "https://schema.org",
+    "@type": "Store",
+    name: "Shoes App",
+    url: "https://yourwebsite.com",
+    description:
+      "Explore our wide range of shoes with the best prices and quality.",
+    image: "https://yourwebsite.com/images/og-image.jpg",
+    potentialAction: {
+      "@type": "SearchAction",
+      target: "https://yourwebsite.com/search?q={search_term_string}",
+      "query-input": "required name=search_term_string",
+    },
+  },
+};
+
 export default function Home() {
   return (
     <div className="text-3xl text-red-700">
